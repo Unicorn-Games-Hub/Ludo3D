d06fef5a09eef0d49654a3eeaf0777e1e74361e9
goops17 <gchitaure@gmail.com>
2021-05-24 07:08:43 +0545

    gameplay screen ui

---
+++ Assets/Scripts/Turntimer/PlayerTurnTimer.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerTurnTimer : MonoBehaviour
+{
+    public int Id;
+    public Text playerNameText;
+    private Image timerIndicator;
+    private Image playerImage;
+    private float turnTimer=1f;
+    private float turnSpeed=10f;
+    public bool myturn=false;
+
+    [Header("Player Bot Icons")]
+    public Sprite[] playerIcon;
+    private int pIndex;
+
+    void Start()
+    {
+        timerIndicator=GetComponent<Image>();
+        playerImage=transform.GetChild(0).GetComponent<Image>();
+        if(GameDataHolder.instance!=null)
+        {
+            pIndex=GameDataHolder.instance.playerIndex[Id];
+            playerImage.sprite=playerIcon[0];
+            if(pIndex==1)
+            {
+                playerImage.sprite=playerIcon[1];
+            }
+        }
+    }
+
+    void Update()
+    {
+        if(myturn)
+        {
+            if(turnTimer>0f)
+            {
+                turnTimer-=Time.deltaTime/turnSpeed;
+                timerIndicator.fillAmount=turnTimer;
+            }
+            else
+            {
+                turnTimer=1f;
+                myturn=false;
+            }
+        }
+    }
+}
